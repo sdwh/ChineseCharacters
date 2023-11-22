e4ef7c21cdc747db053f21c631d4056d530f45b5
Webber <hq@sdwh.dev>
2023-11-22 18:45:40 +0800

    update

--- app.py
+++ app.py
@@ -4,53 +4,54 @@ import readPinyin
 import readStroke
 
 class CharacterInfo:
-    def __init__(self, unicode_number, cns, word):
+    def __init__(self, unicode_number, cns, word, zhuyin, han, zuin2, yale, wei, general, stroke_count):
         self.unicode_number = unicode_number
         self.cns = cns
-        self.zhuyin = None
-        self.han = None
-        self.zuin2 = None
-        self.yale = None
-        self.wei = None
-        self.general = None
-        self.stroke_count = None
+        self.zhuyin = zhuyin
+        self.han = han
+        self.zuin2 = zuin2
+        self.yale = yale
+        self.wei = wei
+        self.general = general
+        self.stroke_count = stroke_count
         self.word = word
 
     def __str__(self):
-        return f"Unicode Number: {self.unicode_number}\nCNS: {self.cns}\n注音: {self.zhuyin}\n漢語: {self.han}\n注音第二式: {self.zuin2}\n耶魯: {self.yale}\n韋式: {self.wei}\n通用: {self.general}\n筆畫數: {self.stroke_count}"
+        return (f"Unicode Number: {self.unicode_number}\nCNS: {self.cns}\n注音: {self.zhuyin}\n漢語: {self.han}\n注音第二式: {self.zuin2}"
+                f"\n耶魯: {self.yale}\n韋式: {self.wei}\n通用: {self.general}\n筆畫數: {self.stroke_count}")
+
+def create_character_info(char):
+    try:
+        unicode_hex = int('0x' + char[0], 16)
+        word = chr(unicode_hex)
+        cns = readCNS.cns_mapping[char[0].upper()]
+        pinyin = readPinyin.phoneticsMapping[cns]
+        stroke = readStroke.strokes[cns]
+
+        return CharacterInfo(
+            unicode_number=char[0].upper(),
+            cns=cns,
+            word=word,
+            zhuyin=pinyin.zhuyin,
+            han=pinyin.han,
+            zuin2=pinyin.zuin2,
+            yale=pinyin.yale,
+            wei=pinyin.wei,
+            general=pinyin.general,
+            stroke_count=stroke
+        )
+    except KeyError as e:
+        print(f"Key error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 res = []
 
-with open('output.txt', 'w', encoding = 'utf8') as file:
-    for c in chars.chars2:
-        try:
-            unicode = str(c[0]).upper()
-            unicode_hex = int('0x' + c[0], 16)
-            word = chr(unicode_hex)
-            cns = readCNS.cns_mapping[unicode]
-            pinyin = readPinyin.phoneticsMapping[cns]
-            stroke = readStroke.strokes[cns]
-            #print(f'{cns}, {chr(unicode_hex)}, {hex(unicode_hex)}, {pinyin.han}')
-            character = CharacterInfo(
-                unicode_number=unicode,
-                cns = cns,
-                word = word
-            )
-            character.zhuyin = pinyin.zhuyin
-            character.han = pinyin.han
-            character.zuin2 = pinyin.zuin2
-            character.yale = pinyin.yale
-            character.wei = pinyin.wei
-            character.general = pinyin.general
-            character.stroke = stroke
+with open('output.txt', 'w', encoding='utf8') as file:
+    for char in chars.chars2:
+        character = create_character_info(char)
+        if character:
             res.append(character)
-
-            line = f'|{word}|{cns}|{unicode}|{pinyin.han}|{pinyin.wei}|{stroke}|\n'
+            line = f'|{character.word}|{character.cns}|{character.unicode_number}|{character.han}|{character.wei}|{character.stroke_count}|\n'
             print(line)
-
-            file.writelines(line)
-        except:
-            print(c)
-
-    
-
+            file.write(line)
